4c5f0ca68679808df5810835a5fb667b5dda0c0e
Narek Asadorian <nasadorian@salesforce.com>
2022-04-19 12:41:47 -0700

    Scala protocol module; Add IronCore SDK (#30)
    * formatting
    
    * Set up Github Actions
    
    * woof
    
    * Change working directory in SBT build steps
    
    * hack.. adding jar to repo
    
    * Fix directory of JAR
    
    * Create dir before cp
    
    * Add filenam
    
    * Add the whole folder baby
    
    * Cleanup
    
    * Publish snapshot of protocol JAR
    
    * Remove JAR
    
    * Update gitignore
    
    * link gitignore
    
    * no link
    
    * fix depth on gitignore
    
    * Remove cp of ironcore JAR
    
    * Add Sonatype
    
    * jars not allowed
    
    * second level gitignore
    
    * add ironcore module
    
    * Fixup IDs
    
    * use sys.env
    
    * clean up TODO

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,6 @@
 FROM openjdk:17-alpine
+ARG FHIR_PROXY_SIGNING_PEM
+ENV JWT_SIGNING_KEY /signing.pem
 ADD target/dbio-fhir-proxy-*SNAPSHOT.jar /app.jar
+RUN echo $FHIR_PROXY_SIGNING_PEM > /signing.pem
 ENTRYPOINT ["java", "-jar", "/app.jar"]
---
+++ protocol-client/src/main/scala/com/dbio/protocol/IronCore.scala
@@ -0,0 +1,31 @@
+package com.dbio.protocol
+
+import cats.effect.IO
+import ironoxide.v1.IronOxide
+import ironoxide.v1.common._
+import ironoxide.v1.user.UserCreateOpts
+
+import scala.concurrent.duration._
+
+object IronCore {
+
+  private val _ = System.loadLibrary("ironoxide_java")
+  private val config = IronOxideConfig(PolicyCachingConfig(666), None)
+  private val deviceOpts = DeviceCreateOpts(DeviceName("fhir-proxy"))
+  private val timeout = Some(3.seconds)
+
+  /**
+  * Authenticate to IronCore for the given user and password.
+  * If the user does not exist in the system, creates them.
+  */
+  def forUser(user: String, pass: String): IO[IronOxide[IO]] =
+    for {
+      jwt <- JwtSigner.forUser(user)
+      verify <- IronOxide.userVerify[IO](jwt, timeout)
+      _ <- IO.whenA(verify.isEmpty)(
+        IronOxide.userCreate[IO](jwt, pass, UserCreateOpts(), timeout).void)
+      device <- IronOxide.generateNewDevice[IO](jwt, pass, deviceOpts, timeout)
+      sdk <- IronOxide.initialize[IO](device.toDeviceContext, config)
+    } yield sdk
+
+}
