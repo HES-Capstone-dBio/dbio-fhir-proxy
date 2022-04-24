d154dd74d30f2d6d595b9c00b3511cca6c2dd261
Narek Asadorian <nasadorian@salesforce.com>
2022-04-24 14:40:37 -0700

    format test file

--- protocol-client/src/test/scala/com/dbio/protocol/TestSuite.scala
+++ protocol-client/src/test/scala/com/dbio/protocol/TestSuite.scala
@@ -1,10 +1,10 @@
 package com.dbio.protocol
 
 import cats.effect.testing.scalatest.AsyncIOSpec
-import org.scalatest.funsuite.AsyncFunSuite
-import org.scalatest.matchers.should.Matchers
 import io.circe.syntax._
 import ironoxide.v1.common.UserId
+import org.scalatest.funsuite.AsyncFunSuite
+import org.scalatest.matchers.should.Matchers
 
 class TestSuite extends AsyncFunSuite with AsyncIOSpec with Matchers {
   test("Round trip encrypt/decrypt with IronCore") {
@@ -12,11 +12,13 @@ class TestSuite extends AsyncFunSuite with AsyncIOSpec with Matchers {
     val prog = for {
       user1 <- IronCore.forUser("testUser1", "password")
       user2 <- IronCore.forUser("testUser2", "password")
-      enc <- IronCore.transferEncrypt(plaintext, UserId("testUser1"), UserId("testUser2")).run(user1)
+      enc <- IronCore
+        .transferEncrypt(plaintext, UserId("testUser1"), UserId("testUser2"))
+        .run(user1)
       out1 <- IronCore.decrypt(enc.encryptedData.toBase64).run(user1)
       out2 <- IronCore.decrypt(enc.encryptedData.toBase64).run(user2)
     } yield out1 -> out2
-    prog.asserting { case (j1, j2) => 
+    prog.asserting { case (j1, j2) =>
       j1 should ===(j2)
       j1 should ===(plaintext)
     }
